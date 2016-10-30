71987c3f27d284318ddc3787c93a0b7537ef379a
DESKTOP-NSQJ6O6\Hari <hari@haridecoded.com>
2016-10-30 00:14:32 -0400

    added code to visualize cigarette usage patterns

--- source/smoker_profiles.R
+++ source/smoker_profiles.R
@@ -6,14 +6,17 @@ install.packages('RColorBrewer')
 install.packages('devtools')
 devtools::install_github("ricardo-bion/ggradar", dependencies=TRUE)
 devtools::install_github("pablo14/clusplus")
+install.packages('fmsb')
 
 library(dplyr)
 library(ggplot2)
 library(MASS)
 library(colorRamps)
 library(RColorBrewer)
-library(ggradar)
+library(fmsb)
 library(clusplus)
+library(scales)
+library(ggradar)
 
 smokers <- samsha %>% filter(CIGFLAG == '(1) Ever used (IRCIGRC = 1-4)') #Selecting for people who have ever smoked
 smokers <- cbind(ID = c(1:nrow(smokers)), smokers) #Adding an ID to link across datasets
@@ -69,6 +72,8 @@ names(profiles)[4:10] =
   c("HEALTH  RISK","ALCOHOL", "HEAVY DRINKING", "RISKY", "NICOTINE", "TREATMENT", "DEPRESSION")
 
 
+#-----------------CLUSTER ANALYSIS---------------------------------------------
+
 #Considered using Principal Component Analysis to see if there were any variables signigicantly driving the difference. However based on the plot, there isn't a marked difference.
 pca <- prcomp(profiles[,4:10],center = TRUE,scale. = TRUE)
 plot(pca, type = "l")
@@ -83,13 +88,155 @@ clusters <- kmeans(profiles[,4:10],8,nstart = 20,algorithm = "Hartigan-Wong")
 # Assign the cluster back to the profiles dataframe
 profiles["CLUSTER"] <- clusters$cluster
 
-#----------------- PARALLEL PLOTS TO VISUALIZE CLUSTERS----------------------
+#----------------- PARALLEL PLOTS TO VISUALIZE CLUSTERS--------------------------
+
 plot_clus_coord(clusters, profiles[,4:10])
 
 
 #----------------- RADAR PLOTS FOR CIGARETTE USE ---------------------------------
 
-cigarettes <- data.frame(smokers[1],smokers[587:589], smokers[682:685], smokers[787:788], smokers[799], smokers[805:808], smokers[810:813])
+#For each of the smoker profile we identified we explore cigarette usage along the following variables:  CIGYR, CIGALCMO, CIGAFU, DCIGAFU, CIGAVGD
+
+#CIGYR: 0 - 1, did not use in past year, used within past year*
+#CIGALCMO : 1 - 4 , c & a, c & !a, !c & a, !c & !a *
+#CIGAFU : 1 - 4, -14, 15- 17, 18 +,  ** 4 is non user, but irrelevent here because of filter condition 
+#DCIGAFU : 1 - 4, -14, 15- 17, 18 +, 4 non-daily user
+#CIGAVGD : 0 - 50 , none/ less than 1, 35 + 
+
+cigarettes <- data.frame(smokers[,c('ID','CIGAVGD')], smokers[,c('CIGYR', 'CIGALCMO', 'CIGAFU', 'DCIGAFU')] %>% 
+                           mutate_each(funs(as.numeric(substr(.,2,2)))))
+
+cigarettes[is.na(cigarettes)] <- 0
+
+cigarettes["CLUSTER"] <- clusters$cluster
+
+#names(cigarettes)[2:6] =
+#  c("DAILY-AVG", " HAS-QUIT", "CIG&ALCOHOL", "AGE-FIRST-USE", "AGE-DAILy-USE")
+
+# groups are of different sizes, so we have to normalize data to make meaningful comparisons between groups on the radar plot
+
+radar_data <- cigarettes %>% 
+  mutate_each(funs(rescale), -CLUSTER,-ID) %>% 
+  group_by(CLUSTER) %>% 
+  summarise(has_quit = mean(CIGYR),first_use_age = mean(CIGAFU),daily_use_age = mean(DCIGAFU), avg_cig = mean(CIGAVGD), cig_alc = mean(CIGALCMO)) %>% 
+  arrange(CLUSTER)
+
+# To use the fmsb package, I have to add 2 lines to the dataframe: the max and min of each topic to show on the plot!
+radar_data=rbind(rep(1,6), rep(0,6) , radar_data)
+
+#------------ CLUSTER 1 ---------------
+
+radarchart( radar_data[1:3,2:6] , axistype=1 , 
+            
+            #custom polygon
+            pcol=rgb(0.2,0.5,0.5,0.9) , pfcol=rgb(0.2,0.5,0.5,0.5) , plwd=2 , 
+            
+            #custom the grid
+            cglcol="grey", cglty=1, axislabcol="grey", cglwd=0.8,
+            
+            #custom labels
+            vlcex=0.8 
+)
+
+#------------ CLUSTER 2 ---------------
+
+radarchart( radar_data[c(1:2,4),2:6] , axistype=1 , 
+            
+            #custom polygon
+            pcol=rgb(0.2,0.5,0.5,0.9) , pfcol=rgb(0.2,0.5,0.5,0.5) , plwd=2 , 
+            
+            #custom the grid
+            cglcol="grey", cglty=1, axislabcol="grey", cglwd=0.8,
+            
+            #custom labels
+            vlcex=0.8 
+)
+
+#------------ CLUSTER 3 ---------------
+
+radarchart( radar_data[c(1:2,5),2:6] , axistype=1 , 
+            
+            #custom polygon
+            pcol=rgb(0.2,0.5,0.5,0.9) , pfcol=rgb(0.2,0.5,0.5,0.5) , plwd=2 , 
+            
+            #custom the grid
+            cglcol="grey", cglty=1, axislabcol="grey", cglwd=0.8,
+            
+            #custom labels
+            vlcex=0.8 
+)
+
+#------------ CLUSTER 4 ---------------
+
+radarchart( radar_data[c(1:2,6),2:6] , axistype=1 , 
+            
+            #custom polygon
+            pcol=rgb(0.2,0.5,0.5,0.9) , pfcol=rgb(0.2,0.5,0.5,0.5) , plwd=2 , 
+            
+            #custom the grid
+            cglcol="grey", cglty=1, axislabcol="grey", cglwd=0.8,
+            
+            #custom labels
+            vlcex=0.8 
+)
+
+#------------ CLUSTER 5 ---------------
+
+radarchart( radar_data[c(1:2,7),2:6] , axistype=1 , 
+            
+            #custom polygon
+            pcol=rgb(0.2,0.5,0.5,0.9) , pfcol=rgb(0.2,0.5,0.5,0.5) , plwd=2 , 
+            
+            #custom the grid
+            cglcol="grey", cglty=1, axislabcol="grey", cglwd=0.8,
+            
+            #custom labels
+            vlcex=0.8 
+)
+
+#------------ CLUSTER 6 ---------------
+
+radarchart( radar_data[c(1:2,8),2:6] , axistype=1 , 
+            
+            #custom polygon
+            pcol=rgb(0.2,0.5,0.5,0.9) , pfcol=rgb(0.2,0.5,0.5,0.5) , plwd=2 , 
+            
+            #custom the grid
+            cglcol="grey", cglty=1, axislabcol="grey", cglwd=0.8,
+            
+            #custom labels
+            vlcex=0.8 
+)
+
+#------------ CLUSTER 7 ---------------
+
+radarchart( radar_data[c(1:2,9),2:6] , axistype=1 , 
+            
+            #custom polygon
+            pcol=rgb(0.2,0.5,0.5,0.9) , pfcol=rgb(0.2,0.5,0.5,0.5) , plwd=2 , 
+            
+            #custom the grid
+            cglcol="grey", cglty=1, axislabcol="grey", cglwd=0.8,
+            
+            #custom labels
+            vlcex=0.8 
+)
+
+#------------ CLUSTER 8 ---------------
+
+radarchart( radar_data[c(1:2,10),2:6] , axistype=1 , 
+            
+            #custom polygon
+            pcol=rgb(0.2,0.5,0.5,0.9) , pfcol=rgb(0.2,0.5,0.5,0.5) , plwd=2 , 
+            
+            #custom the grid
+            cglcol="grey", cglty=1, axislabcol="grey", cglwd=0.8,
+            
+            #custom labels
+            vlcex=0.8 
+)
+
+
 
 
 
